804769e5916619296d354a6241029afa18d76622
cwebster-99 <clw99@swbell.net>
2024-08-21 13:24:34 -0500

    Minor changes

--- Notebook/generate_visual.py
+++ Notebook/generate_visual.py
@@ -1,22 +1,20 @@
-# visualize data based on geographical info
 import matplotlib.image as mpimg
-import matplotlib.pyplot as mppyplot
 import matplotlib.pyplot as plt
 import numpy as np
 
 def population_price_visual(df):
-    california_img=mpimg.imread('../images/california.png')
-    ax = df.plot(kind="scatter", x="longitude", y="latitude", figsize=(10,7),
-                    s=df['population']/100, label="Population",
-                    c="median_house_value", cmap=plt.get_cmap("jet"),
-                    colorbar=False, alpha=0.4)
+    california_img = mpimg.imread('C:\CaliforniaHousingPrediction\images\california.png')
+    df.plot(kind="scatter", x="longitude", y="latitude", figsize=(10, 7),
+                 s=df['population'] / 100, label="Population",
+                 c="median_house_value", cmap=plt.get_cmap("jet"),
+                 colorbar=False, alpha=0.4)
     plt.imshow(california_img, extent=[-124.55, -113.80, 32.45, 42.05], alpha=0.5,
-            cmap=plt.get_cmap("jet"))
+               cmap=plt.get_cmap("jet"))
     plt.ylabel("Latitude", fontsize=14)
     plt.xlabel("Longitude", fontsize=14)
 
     prices = df["median_house_value"]
-    tick_values = np.linspace(prices.min(), prices.max(), 11)
-
+    np.linspace(prices.min(), prices.max(), 11)
+    
     population_price_graph = plt.show()
     return population_price_graph
